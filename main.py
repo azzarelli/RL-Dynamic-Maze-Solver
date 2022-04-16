a0c6ebe1906f521cce3b1f791577e9375128c6df
Adrian <aab1g18@soton.ac.uk>
2022-04-16 19:19:26 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from run import run
 
 if __name__ == '__main__':
-    iter = [0.1,0.01,0.001,0.0001]
+    iter = [1]
 
     for i in iter:
         run(canv_chck=True, chckpt=False, episodes=1000,
             lr=0.001, epsilon=0.9, gamma=0.99,
-           netname='variations', batch_size=64, epsilon_min=0.2, ep_dec=0.01)
+           netname='variations', batch_size=16, epsilon_min=0.5, beta_inc=0.1, ep_dec=0.01)
