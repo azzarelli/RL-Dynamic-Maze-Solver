cfb994eb818f33ecdb903be4151bc96c0605546c
Adrian <aab1g18@soton.ac.uk>
2022-04-16 19:16:48 +0100

    PER updates

--- classes/agent.py
+++ classes/agent.py
@@ -1,17 +1,13 @@
 import numpy as np
 import torch as T
 import time
-from classes.replaybuffer import ReplayBuffer # Simple Replay Buffer
+from classes.replaybuffer import PrioritizedBuffer # Simple Replay Buffer
 #from classes.replaybuffer_ import ReplayBuffer
 
 from classes.ddqn import DDQN
+from classes.convddqn import ConvDDQN
+from torchvision.transforms import  transforms
 
-def TD_errors(q_pred, q_tru):
-    return q_pred - q_tru
-
-def get_priorities(q_pred, q_tru, err=1e-6):
-    with T.no_grad():
-        return np.abs(TD_errors(q_pred, q_tru).cpu().numpy()) + err
 
 class Agent():
     def __init__(self, gamma, epsilon, lr, n_actions, input_dims,
@@ -39,12 +35,13 @@ class Agent():
 
         self.action_space = [i for i in range(self.n_actions)]
 
-        self.memory = ReplayBuffer(input_dims,  batch_size, mem_size, self.alpha)
+        self.memory = PrioritizedBuffer(mem_size, self.batch_size, self.alpha, self.beta)
 
-        self.q_eval = DDQN(self.lr, self.n_actions, input_dims=input_dims,
+        self.q_eval = DDQN(self.lr, self.n_actions, input_dim=input_dims,
                            name=name, save_dir=self.save_dir)
-        self.q_next = DDQN(self.lr, self.n_actions, input_dims=input_dims,
+        self.q_next = DDQN(self.lr, self.n_actions, input_dim=input_dims,
                            name=name+'.next', save_dir=self.save_dir)
+        self.q_next.eval()
 
 
     def greedy_epsilon(self, observation):
@@ -52,11 +49,12 @@ class Agent():
             actions = T.Tensor([])
             # if we randomly choose max expected reward action
             if np.random.random() > self.epsilon:
-                state = T.tensor(observation, dtype=T.float).to(self.q_eval.device)
-                _, advantage = self.q_eval.forward(state)
-                #print(advantage, T.argmax(advantage).item())
-                action = T.argmax(advantage).item()
-                actions = advantage
+                #state = (observation).to(self.q_eval.device)
+                state = T.FloatTensor(observation).float().unsqueeze(0).to(self.q_eval.device)
+                # state = T.tensor([observation], dtype=T.float).to(self.q_eval.device)
+                Q = self.q_eval.forward(state)
+                action = np.argmax(Q.cpu().detach().numpy())
+                actions = Q
             # otherwise random action
             else:
                 action = np.random.choice(self.action_space)
@@ -70,16 +68,16 @@ class Agent():
             print('Update target network...')
             self.q_next.load_state_dict(self.q_eval.state_dict())
 
-    def step_params(self):
+    def step_params(self,b):
         self.dec_epsilon()
-        self.inc_beta()
+        self.inc_beta(b)
 
     def dec_epsilon(self):
         self.epsilon = self.epsilon - self.eps_dec \
             if self.epsilon > self.eps_min else self.eps_min
 
-    def inc_beta(self):
-        self.beta = self.beta + 0.001 if self.beta < 1 else 1
+    def inc_beta(self, b):
+        self.beta = self.beta + b if self.beta < 1 else 1
 
     def save_models(self):
         self.q_eval.save_()
@@ -89,99 +87,64 @@ class Agent():
         self.q_eval.load_save()
         self.q_next.load_save()
 
+    def compute_loss(self):
+        # sample memory (state_batch, action_batch, reward_batch, next_state_batch, done_batch), batch_idx, IS_weights
+        state, actions, reward, state_, term, weights, batch_idxs = self.memory.sample()
+
+        #sample = self.memory.sample()
+        # state = sample['state']
+        # state_ = sample['state_']
+        # actions = sample['action']
+        # reward = sample['reward']
+        # term = sample['done']
+
+        states = T.FloatTensor(state).to(self.q_eval.device)
+        actions = T.LongTensor(actions).type(T.int64) .to(self.q_eval.device)
+        term = T.BoolTensor(term).to(self.q_eval.device)
+        rewards = T.FloatTensor(reward).to(self.q_eval.device)
+        states_ = T.FloatTensor(state_).to(self.q_eval.device)
+
+        weights = T.FloatTensor(weights).to(self.q_eval.device)
+
+        Q_pred = self.q_eval.forward(states).gather(1, actions.unsqueeze(1)).squeeze(1)
+        Q_next = self.q_next.forward(states_)
+
+        q_pred = Q_pred
+        q_next = Q_next
+
+        q_target = rewards.squeeze(1) + self.gamma * T.max(q_next, dim=1)[0] #.detach() #q_next[idxs, max_actions]
+
+        q_target[term] = 0.0
+
+        # Loss
+        # loss = self.q_eval.loss(q_target, q_pred).to(self.q_eval.device)
+        #w = T.unsqueeze(weights, 1).to(self.q_eval.device)
+        # loss = (loss * w).mean()
+
+        td_errors = T.pow(q_pred - q_target, 2) * weights
+
+
+        return td_errors, batch_idxs
+
     def learn(self):
         # Wait for memory to fill up before learning from empty set
         if not self.memory.is_sufficient():
             return
 
         #time.sleep(5) # delay for time animation
-
-        # Start AD
-        #self.replace_target_network()
-
-
-        # sample memory
-        sample = self.memory.sample(self.beta)
-        state = sample['state']
-        state_ = sample['state_']
-        actions = sample['action']
-        reward = sample['reward']
-        term = sample['done']
-
-        states = T.tensor(state).to(self.q_eval.device)
-        actions = T.tensor(actions).to(self.q_eval.device)
-        term = T.tensor(term).to(self.q_eval.device)
-        rewards = T.tensor(reward).to(self.q_eval.device)
-        states_ = T.tensor(state_).to(self.q_eval.device)
-
-        V_s, A_s = self.q_eval.forward(states)
-        V_s_, A_s_ = self.q_next.forward(states_)
-        # V_s_eval, A_s_eval = self.q_eval.forward(states_)
-
-        q_pred = T.add(V_s, (A_s - A_s.mean(dim=1, keepdim=True))).gather(1,
-                                            actions.unsqueeze(-1)).squeeze(-1) #[idxs, actions]
-
-        q_next = T.add(V_s_, (A_s_ - A_s.mean(dim=1, keepdim=True)))
-
-        q_target = rewards + self.gamma * T.max(q_next, dim=1)[0].detach() #q_next[idxs, max_actions]
-        q_target[term] = 0.0
-        #print(q_target)
-
-        loss = self.q_eval.loss(q_target, q_pred).to(self.q_eval.device)
-        w = T.unsqueeze(T.Tensor(sample['weights']), 1).to(self.q_eval.device)
-
-        loss = (loss * w).mean()
+        loss, idxs = self.compute_loss()
+        lossmean = loss.mean()
 
         self.q_eval.optimiser.zero_grad()
-        #loss = loss.to(self.q_eval.device)
-        loss.backward()
+        lossmean.backward()
 
-        priorities = get_priorities(q_pred, q_target)
-        # print(priorities)
-        self.memory.update_priorities(sample['indexes'], priorities)
-
-        #T.nn.utils.clip_grad_norm_(self.q_eval.parameters(), max_norm=0.5)
+        T.nn.utils.clip_grad_norm_(self.q_eval.parameters(), max_norm=0.5)
 
         self.q_eval.optimiser.step()
         self.learn_step_counter += 1
 
-        # self.dec_epsilon
-        # self.inc_beta # beta tends to 1 as training goes on
+        # update priorities
+        for idx, td_error in zip(idxs, loss.cpu().detach().numpy()):
+            self.memory.update_priorities(idx, td_error)
 
-        return loss
-
-
-    def test(self):
-        with T.no_grad():
-            self.replace_target_network()
-
-            # sample memory
-            state, actions, reward, state_, term = \
-                    self.memory.sample_buffer()
-
-            states = T.tensor(state).to(self.q_eval.device)
-            actions = T.tensor(actions).to(self.q_eval.device)
-            term = T.tensor(term).to(self.q_eval.device)
-            rewards = T.tensor(reward).to(self.q_eval.device)
-            states_ = T.tensor(state_).to(self.q_eval.device)
-
-            idxs = np.arange(self.batch_size)
-
-            V_s, A_s = self.q_eval.forward(states)
-            V_s_, A_s_ = self.q_next.forward(states_)
-            V_s_eval, A_s_eval = self.q_eval.forward(states_)
-
-            curr_q = T.add(V_s, (A_s - A_s.mean(dim=1, keepdim=True)))[idxs, actions]
-            next_q = T.add(V_s_,(A_s_ - A_s.mean(dim=1, keepdim=True)))
-
-            next_eval = T.add(V_s_eval, (A_s_eval - A_s_eval.mean(dim=1, keepdim=True)))
-
-            max_actions = T.argmax(next_eval, dim=1)
-            print(max_actions)
-            # apply mask for terminates networks
-            next_eval[term] = -10.0
-
-            q_target = rewards + self.gamma * next_q[idxs, max_actions]
-
-            loss = self.q_eval.loss(q_target, curr_q).to(self.q_eval.device)
         return loss
